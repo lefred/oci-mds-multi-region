ebd9a0e368bd4a233efa6cbbe41c698d9ac173ce
Frederic Descamps <lefred.descamps@gmail.com>
2021-10-14 12:42:18 +0200

    add some extra tools
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ locals {
   public_subnet_id_source = var.existing_public_subnet_ocid_source == "" ? oci_core_subnet.public1[0].id : var.existing_public_subnet_ocid_source
   private_security_list_id_source = var.existing_private_security_list_ocid_source == "" ? oci_core_security_list.private_security_list1[0].id : var.existing_private_security_list_ocid_source
   public_security_list_id_source = var.existing_public_security_list_ocid_source == "" ? oci_core_security_list.public_security_list1[0].id : var.existing_public_security_list_ocid_source
+
   vcn_id_replica = var.existing_vcn_ocid_replica == "" ? oci_core_virtual_network.mysqlvcn2[0].id : var.existing_vcn_ocid_replica
   internet_gateway_id_replica = var.existing_internet_gateway_ocid_replica == "" ? oci_core_internet_gateway.internet_gateway2[0].id : var.existing_internet_gateway_ocid_replica
   nat_gateway_id_replica = var.existing_nat_gateway_ocid_replica == "" ? oci_core_nat_gateway.nat_gateway2[0].id : var.existing_nat_gateway_ocid_replica
@@ -18,6 +19,8 @@ locals {
   public_subnet_id_replica = var.existing_public_subnet_ocid_replica == "" ? oci_core_subnet.public2[0].id : var.existing_public_subnet_ocid_replica
   private_security_list_id_replica = var.existing_private_security_list_ocid_replica == "" ? oci_core_security_list.private_security_list2[0].id : var.existing_private_security_list_ocid_replica
   public_security_list_id_replica = var.existing_public_security_list_ocid_replica == "" ? oci_core_security_list.public_security_list2[0].id : var.existing_public_security_list_ocid_replica
+
+  # NEW REGION
 
   ssh_key = var.ssh_authorized_keys_path == "" ? tls_private_key.public_private_key_pair.public_key_openssh : file(var.ssh_authorized_keys_path)
   ssh_private_key = var.ssh_private_key_path == "" ? tls_private_key.public_private_key_pair.private_key_pem : file(var.ssh_private_key_path)
@@ -54,6 +57,7 @@ data "oci_core_images" "images_for_shape_replica" {
     provider = oci.replica
 }    
     
+# NEW REPLICA IMAGE
 
 data "oci_mysql_mysql_configurations" "shape" {
     compartment_id = var.compartment_ocid
@@ -113,6 +117,8 @@ resource "oci_core_route_table" "public_route_table1" {
         network_entity_id = oci_core_drg.mds_drg_source.id
   }
 
+  # NEW REPLICA ROUTE RULES
+
   provider = oci.source
 
   count = var.existing_public_route_table_ocid_source == "" ? 1 : 0
@@ -153,6 +159,8 @@ resource "oci_core_route_table" "private_route_table1" {
         destination_type  = "CIDR_BLOCK"
         network_entity_id = oci_core_drg.mds_drg_source.id
   }
+
+  # NEW REPLICA ROUTE RULES
 
   count = var.existing_private_route_table_ocid_source == "" ? 1 : 0
   provider = oci.source
@@ -211,6 +219,7 @@ resource "oci_core_security_list" "private_security_list1" {
     protocol = "6"
     source   = var.vcn_cidr_replica
   }
+  # NEW INGRESS SECURITY RULES 
   provider = oci.source
 
   count = var.existing_private_security_list_ocid_source == "" ? 1 : 0
@@ -258,205 +267,3 @@ module "mds-instance" {
   deploy_ha = var.deploy_mds_ha
 }
 
-// Replica
-
-resource "oci_core_subnet" "public2" {
-  cidr_block = cidrsubnet(var.vcn_cidr_replica, 8, 0)
-  display_name = "mysql_public_subnet2"
-  compartment_id = var.compartment_ocid
-  vcn_id = local.vcn_id_replica
-  route_table_id = local.public_route_table_id_replica
-//  security_list_ids = [local.public_security_list_id_replica]
-  dns_label = "mysqlpub2"
-  provider = oci.replica
-
-  count = var.existing_public_subnet_ocid_replica == "" ? 1 : 0
-}
-
-data "oci_identity_availability_domains" "ad_replica" {
-  compartment_id = var.tenancy_ocid
-  provider = oci.replica
-}
-
-data "template_file" "ad_names_replica" {
-  count    = length(data.oci_identity_availability_domains.ad_replica.availability_domains)
-  template = lookup(data.oci_identity_availability_domains.ad_replica.availability_domains[count.index], "name")
-}
-
-resource "oci_core_virtual_network" "mysqlvcn2" {
-  cidr_block = var.vcn_cidr_replica
-  compartment_id = var.compartment_ocid
-  display_name = var.vcn_replica
-  dns_label = "mysqlvcn2"
-  provider = oci.replica
-
-  count = var.existing_vcn_ocid_replica == "" ? 1 : 0
-}
-
-
-resource "oci_core_internet_gateway" "internet_gateway2" {
-  compartment_id = var.compartment_ocid
-  display_name = "internet_gateway_2"
-  vcn_id = local.vcn_id_replica
-  provider = oci.replica
-
-  count = var.existing_internet_gateway_ocid_replica == "" ? 1 : 0
-}
-
-
-resource "oci_core_nat_gateway" "nat_gateway2" {
-  compartment_id = var.compartment_ocid
-  vcn_id = local.vcn_id_replica
-  display_name   = "nat_gateway2"
-  provider = oci.replica
-
-  count = var.existing_nat_gateway_ocid_replica == "" ? 1 : 0
-}
-
-
-resource "oci_core_route_table" "public_route_table2" {
-  compartment_id = var.compartment_ocid
-  vcn_id = local.vcn_id_replica
-  display_name = "RouteTableForMySQLPublic2"
-  route_rules {
-    destination = "0.0.0.0/0"
-    network_entity_id = local.internet_gateway_id_replica
-  }
-  route_rules {
-        destination       = var.vcn_cidr_source
-        destination_type  = "CIDR_BLOCK"
-        network_entity_id = oci_core_drg.mds_drg_replica.id
-  }
-  provider = oci.replica
-
-  count = var.existing_public_route_table_ocid_replica == "" ? 1 : 0
-}
-
-resource "oci_core_security_list" "public_security_list2" {
-  compartment_id = var.compartment_ocid
-  display_name = "Allow Public SSH Connections to Eventual Servers in Public Subnet"
-  vcn_id = local.vcn_id_replica
-  egress_security_rules {
-    destination = "0.0.0.0/0"
-    protocol = "6"
-  }
-  ingress_security_rules {
-    tcp_options {
-      max = 22
-      min = 22
-    }
-    protocol = "6"
-    source   = "0.0.0.0/0"
-  }
-  provider = oci.replica
-
-  count = var.existing_public_security_list_ocid_replica == "" ? 1 : 0
-}
-
-resource "oci_core_route_table" "private_route_table2" {
-  compartment_id = var.compartment_ocid
-  vcn_id = local.vcn_id_replica
-  display_name   = "RouteTableForMySQLPrivate2"
-  route_rules {
-    destination       = "0.0.0.0/0"
-    network_entity_id = local.nat_gateway_id_replica
-  }
-  route_rules {
-        destination       = var.vcn_cidr_source
-        destination_type  = "CIDR_BLOCK"
-        network_entity_id = oci_core_drg.mds_drg_replica.id
-  }
-
-  count = var.existing_private_route_table_ocid_replica == "" ? 1 : 0
-  provider = oci.replica
-}
-
-resource "oci_core_security_list" "private_security_list2" {
-  compartment_id = var.compartment_ocid
-  display_name   = "Private2"
-  vcn_id = local.vcn_id_replica
-
-  egress_security_rules {
-    destination = "0.0.0.0/0"
-    protocol    = "all"
-  }
-  ingress_security_rules  {
-    protocol = "1"
-    source   = var.vcn_cidr_replica
-  }
-  ingress_security_rules  {
-    tcp_options  {
-      max = 22
-      min = 22
-    }
-    protocol = "6"
-    source   = var.vcn_cidr_replica
-  }
-  ingress_security_rules  {
-    tcp_options  {
-      max = 3306
-      min = 3306
-    }
-    protocol = "6"
-    source   = var.vcn_cidr_replica
-  }
-  ingress_security_rules  {
-    tcp_options  {
-      max = 33061
-      min = 33060
-    }
-    protocol = "6"
-    source   = var.vcn_cidr_replica
-  }
-  ingress_security_rules  {
-    tcp_options  {
-      max = 3306
-      min = 3306
-    }
-    protocol = "6"
-    source   = var.vcn_cidr_source
-  }
-  ingress_security_rules  {
-    tcp_options  {
-      max = 33061
-      min = 33060
-    }
-    protocol = "6"
-    source   = var.vcn_cidr_source
-  }
-  provider = oci.replica
-
-  count = var.existing_private_security_list_ocid_replica == "" ? 1 : 0
-}
-
-resource "oci_core_subnet" "private2" {
-  cidr_block                 = cidrsubnet(var.vcn_cidr_replica, 8, 1)
-  display_name               = "mysql_private_subnet2"
-  compartment_id             = var.compartment_ocid
-  vcn_id                     = local.vcn_id_replica
-  route_table_id             = local.private_route_table_id_replica
-  security_list_ids          = [local.private_security_list_id_replica]
-  prohibit_public_ip_on_vnic = "true"
-  dns_label                  = "mysqlpriv2"
-  provider = oci.replica
-
-  count = var.existing_private_subnet_ocid_replica == "" ? 1 : 0
-
-}
-
-module "mds-instance_replica" {
-  
-  providers = {
-    oci = oci.replica
-  }
-  source         = "./modules/mds-instance"
-  admin_password = var.admin_password
-  admin_username = var.admin_username
-  availability_domain = data.template_file.ad_names_replica.*.rendered[0]
-  configuration_id = data.oci_mysql_mysql_configurations.shape.configurations[0].id
-  compartment_ocid = var.compartment_ocid
-  subnet_id = local.private_subnet_id_replica
-  display_name = "${var.mds_instance_name}_Replica"
-  existing_mds_instance_id  = var.existing_mds_instance_ocid_replica
-  mysql_shape = var.mysql_shape
-}
---
+++ tools/add_new_region.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+echo adding a new region...
+
+# get the amount of replica(s)
+
+replica_nb=$(grep "vcn_id_replica.* = " main.tf | wc -l)
+replica_nb=$((replica_nb + 1))
+route_nb=$((replica_nb + 1))
+new="$(sed 's/2/'$route_nb'/g' tools/extra/locals)"
+new=$(echo "$new" | sed 's/replica/replica'$replica_nb'/g')
+tmpfile=$(mktemp)
+echo "$new" > $tmpfile
+sed -i "/# NEW REGION/r $tmpfile" main.tf
+
+new="$(sed 's/replica/replica'$replica_nb'/g' tools/extra/shape)"
+echo "$new" > $tmpfile
+sed -i "/# NEW REPLICA IMAGE/r $tmpfile" main.tf
+
+new="$(sed 's/NBREPLICA/'$replica_nb'/g' tools/extra/routerules)"
+echo "$new" > $tmpfile
+sed -i "/# NEW REPLICA ROUTE RULES/r $tmpfile" main.tf
+
+new="$(sed 's/replica/replica'$replica_nb'/g' tools/extra/securityrules)"
+echo "$new" > $tmpfile
+sed -i "/# NEW INGRESS SECURITY RULES/r $tmpfile" main.tf
+
+new="$(sed 's/replica/replica'$replica_nb'/g' tools/extra/variables)"
+echo >> variables.tf
+echo "$new" >> variables.tf 
+
+cp replica.tf replica${replica_nb}.tf
+sed -i "s/2/$route_nb/g" replica${replica_nb}.tf
+sed -i "s/replica/replica$replica_nb/g" replica${replica_nb}.tf
+sed -i "s/Replica/Replica$replica_nb/g" replica${replica_nb}.tf
+
+cp tools/extra/drg drg${replica_nb}.tf
+sed -i "s/NBREPLICA/$replica_nb/g" drg${replica_nb}.tf
+
+cp inbound.tf inbound${replica_nb}.tf
+sed -i "s/replica/replica$replica_nb/g" inbound${replica_nb}.tf
+
+cat << EOF >> output.tf
+
+output "mds_instance_ip_replica${replica_nb}" {
+  value = module.mds-instance_replica${replica_nb}.private_ip
+}
+EOF
+
+cat << EOF2 >> provider.tf
+
+provider "oci" {
+  region = var.region_replica${replica_nb}
+  user_ocid = var.user_ocid
+  fingerprint = var.fingerprint
+  private_key_path = var.private_key_path
+  tenancy_ocid = var.tenancy_ocid
+  alias = "replica${replica_nb}"
+}
+EOF2
+
+echo "region_replica2 = \"ap-tokyo-1\"" >> terraform.tvars
+echo "vcn_cidr_replica2 = \"10.${replica_nb}.0.0/16\"" >> terraform.tfvars 
